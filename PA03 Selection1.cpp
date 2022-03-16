5b5ef8e8cdff648b6f21697a0193b1f9a61c410b
TimothyByrd3rd <101755754+TimothyByrd3rd@users.noreply.github.com>
2022-03-16 17:46:58 -0500

    Add files via upload

---
+++ PA03 Selection1.cpp
@@ -0,0 +1,25 @@
+/*
+Timothy Byrd
+Fall 2019
+CM 210 Computer Science I with C++
+October 9, 2019
+
+This program implements an 'if' selection structure 
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  //the if structure
+  int x = 7;
+  int y = 4;
+
+  if(x > y)
+  {
+    cout<< "x is greater than y\n";
+  }
+
+  return 0;
+}
